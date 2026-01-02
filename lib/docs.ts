003124164839fa336c0de05e0255b6a747b6405a
Wang Jiahua <jhwang0324@gmail.com>
2026-01-02 16:13:27 +0800

    docs: added heatmap to new components

--- lib/docs.ts
+++ lib/docs.ts
@@ -1,6 +1,7 @@
 export const PAGES_NEW = [
   "/docs/components/flip-clock",
-  "/docs/components/partition-bar"
+  "/docs/components/partition-bar",
+  "/docs/components/heatmap"
 ];
 
 export const PAGES_UPDATED = [];
